6e9e5f70694e2880b05cb1391abd5378f4f7fa8a
lorick-tsilavina <lorick.tsarazaka.lk@gmail.com>
2025-07-14 13:20:40 +0300

    lk

--- pages/login.php
+++ pages/login.php
@@ -1,17 +1,17 @@
-<section>
-    <div class="col-10 m-auto">
-        <h2 class="text-center mb-2">Login</h2>
-        <form action="traitement_login.php" method="post">
-            <div class="mb-3">
+<section class="d-flex flex-column justify-content-center align-items-center" style="min-height:90vh;">
+    <div class="col-10 col-md-5 m-auto p-4 rounded-4 form-login">
+        <h1 class="text-center mb-2">Login</h1>
+        <form action="traitement_login.php" class="d-flex flex-column align-items-center" method="post">
+            <div class="mb-3 w-100">
                 <label for="" class="form-label">Email address</label>
                 <input type="email" name="email" class="form-control" id="" placeholder="Email" required>
             </div>
-            <div class="mb-3">
+            <div class="mb-3 w-100">
                 <label for="" class="form-label">Password</label>
                 <input type="password" name="mdp" class="form-control" id="" placeholder="Password" required>
             </div>
             <div class="d-grid gap-2">
-                <input type="submit" value="Login" class="btn btn-primary">
+                <input type="submit" value="Login" class="bouton rounded-5 fw-bold fs-5 p-1 ps-5 pe-5 btn btn-primary">
             </div>
         </form>
     </div>
---
+++ run_lk.sh
@@ -0,0 +1,2 @@
+rm -rf /opt/lampp/htdocs/itu.S2.web.final-project
+cp -rf /home/lorick/Documents/GitHub/itu.S2.web.final-project /opt/lampp/htdocs/
